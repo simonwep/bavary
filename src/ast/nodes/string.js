6130f24d0cb285528928e90d1826a9174d5bc43c
Simon Reinisch <simon@reinisch.io>
2019-10-28 20:58:15 +0100

    Throw error if empty string is used

--- src/ast/nodes/string.js
+++ src/ast/nodes/string.js
@@ -4,6 +4,10 @@ const maybe = require('../tools/maybe');
 module.exports = maybe(stream => {
     const string = optional(stream, 'str');
 
+    if (string && !string.value.length) {
+        return stream.throwError(`Strings shouldn't be empty.`);
+    }
+
     return string ? {
         type: 'string',
         value: string.value
