9dd4da0de77bd147c5fc9cd0022d783a2facf0c4
Simon Reinisch <simon@reinisch.io>
2019-11-04 17:21:25 +0100

    Create extra test-suite just for combinators

--- test/compiler/nested-groups.test.js
+++ test/compiler/nested-groups.test.js
@@ -33,30 +33,4 @@ describe('[COM] Nested groups', () => {
         expect(parse('')).to.deep.equal([]);
         expect(parse('AC')).to.deep.equal(null);
     });
-
-    it('Should handle mixed combinators', () => {
-        const parse = compile(`
-            entry {
-                <num> = ['0' to '9']
-                <abc> = ['a' to 'c']
-                <xyz> = ['x' to 'z']
-            
-                default [
-                    [<num#a> && <abc#b> && <xyz#c>] | 
-                    ['a' & 'b' & 'c'] | 
-                    ['e' | 'f'] | 
-                    ['g' 'h']
-                ]
-            }
-        `);
-
-        expect(parse('0x')).to.deep.equal({a: '0', b: null, c: 'x'});
-        expect(parse('z')).to.deep.equal({a: null, b: null, c: 'z'});
-        expect(parse('y0b')).to.deep.equal({a: '0', b: 'b', c: 'y'});
-        expect(parse('gh')).to.equal('gh');
-        expect(parse('e')).to.equal('e');
-        expect(parse('abbc')).to.equal(null);
-        expect(parse('cca')).to.equal(null);
-        expect(parse('hg')).to.equal(null);
-    });
 });
