ebb1f3034562976288b0a8b9f1cfcaeaff1ccdef
Simon Reinisch <simon@reinisch.io>
2019-11-28 19:00:44 +0100

    Update tag parser
    Identifier returns not longer an object

--- src/core/ast/nodes/tag.ts
+++ src/core/ast/nodes/tag.ts
@@ -1,19 +1,19 @@
 import {maybe}    from '../tools/maybe';
 import {optional} from '../tools/optional';
-import {Tag}    from '../types';
+import {Tag}      from '../types';
 
 module.exports = maybe<Tag | null>(stream => {
     if (!optional(stream, 'punc', '#')) {
         return null;
     }
 
-    const ident = require('./identifier')(stream);
+    const ident = require('../modifiers/identifier')(stream);
     if (!ident) {
         stream.throwError('Expected tag-identifier');
     }
 
     return {
         type: 'tag',
-        value: ident.value
+        value: ident
     };
 });
