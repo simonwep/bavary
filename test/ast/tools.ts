95211a6c20006a45f1c2db5d0616584b55cfb0a3
Simon Reinisch <simon@reinisch.io>
2020-01-05 19:08:31 +0100

    Fix broken tools in ast-tests

--- test/ast/tools.ts
+++ test/ast/tools.ts
@@ -1,9 +1,9 @@
-import {expect}      from 'chai';
-import {parse}       from '../../src/ast';
-import {Declaration} from '../../src/ast/types';
-import {tokenize}    from '../../src/tokenizer';
+import {expect}            from 'chai';
+import {parse as parseAST} from '../../src/ast';
+import {Declaration}       from '../../src/ast/types';
+import {tokenize}          from '../../src/tokenizer';
 
-export const parse = (str: string): Array<Declaration> => parse(tokenize(str), str);
+export const parse = (str: string): Array<Declaration> => parseAST(tokenize(str), str);
 export const failAll = (tests: Array<string>): void => {
     for (const str of tests) {
         it(`Should throw an error for ${str}`, () => {
